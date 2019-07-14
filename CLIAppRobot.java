1c4b2c9f804b81ad986830de468ece26f80b276f
Richardo Alvin <richardo.alvin@yahoo.com>
2019-07-14 21:27:17 +1000

    Modifying the input of the application

--- CLIAppRobot.java
+++ CLIAppRobot.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+
 public class CLIAppRobot{
   public static void main(String[] args){
-    System.out.println("input: " + args[0]);
+    // remove the first and the last quatation
+    String text = args[0].substring(1, args[0].length() - 1);
+    // split comma into array list
+    List<String> items = Arrays.asList(text.split("\\s*,\\s*"));
+    StringBuilder sb = new StringBuilder();
+    // combining array into a single StringBuilder
+     for (String ch : items) {
+       sb.append(ch);
+     }
+    System.out.println("input: " + sb);
   }
 }
